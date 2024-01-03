3fe4728b62025199a165020a9098086459c401fe
Jeongchanyeong <135792468020@naver.com>
2024-01-03 22:51:43 +0900

    Feat/ header 날짜 변경 구현 , dummyList -> Home 나열 환경 구축

--- src/App.js
+++ src/App.js
@@ -4,48 +4,117 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
+import React, { useReducer, useRef } from "react";
 
-// COMPONENTS
-import MyButton from "./components/MyButton";
-import MyHeader from "./components/MyHeader";
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+const dummyList = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "일기 1번",
+    date: 1704260575375,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "일기 2번",
+    date: 1704260575376,
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "일기 3번",
+    date: 1704260575377,
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "일기 4번",
+    date: 1704260575378,
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "일기 5번",
+    date: 1704260575379,
+  },
+];
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, dummyList);
+  const dataId = useRef(0);
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+  // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader
-          headText={"감정 일기장"}
-          leftChild={
-            <MyButton text={"좌측"} onClick={() => alert("왼쪽 클릭")} />
-          }
-          rightChild={
-            <MyButton text={"우측"} onClick={() => alert("오른쪽 클릭")} />
-          }
-        />
-        <h2>App.js</h2>
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"positive"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"negative"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"default"}
-        />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/diary/" element={<Diary />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={(onCreate, onEdit, onRemove)}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/diary/" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
