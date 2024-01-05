650eefc3d558274663f8636102b0988bb117887d
Jeongchanyeong <135792468020@naver.com>
2024-01-05 18:16:56 +0900

    Feat/  Home 최신순, 감정 정도 filter 구현

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -5,6 +5,12 @@ const sortOptionList = [
   { value: "oldest", name: "오래된 순" },
 ];
 
+const emotionFilterList = [
+  { value: "all", name: "모든 감정" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안 좋은 감정만" },
+];
+
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
     <select value={value} onChange={(e) => onChange(e.target.value)}>
@@ -19,9 +25,18 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("latest");
+  const [emotionFilter, setEmotionFilter] = useState("all");
 
   // 최신순인지 오래된 순인지 if문으로 분기처리 후 정렬된 리스트를 반환
   const getProcessedDiaryList = () => {
+    const filterCallback = (it) => {
+      if (emotionFilter === "good") {
+        return parseInt(it.emotion) <= 3;
+      } else {
+        return parseInt(it.emotion) > 3;
+      }
+    };
+
     const compare = (a, b) => {
       if (sortType === "latest") {
         // 문자열이 들어올 수 있기 때문에 parseInt 사용
@@ -33,19 +48,36 @@ const DiaryList = ({ diaryList }) => {
     // 최신, 오래된 순으로 정렬하는데 배열내장함수 sort를 사용하게 되면 원본배열 자체가 정렬되기 떄문에깊은 복사를 통해 데이터를 붙여넣음
     // 배열인 diaryList를 문자열로 반환되어 JSON.parse를 통해 배열로 바뀌어 copyList에 저장
     const copyList = JSON.parse(JSON.stringify(diaryList));
-    const sortedList = copyList.sort(compare);
+
+    const filteredList =
+      emotionFilter === "all"
+        ? copyList
+        : copyList.filter((it) => filterCallback(it));
+
+    const sortedList = filteredList.sort(compare);
     return sortedList;
   };
 
   return (
     <div>
+      {/* 최신, 오래된 순 filter select */}
       <ControlMenu
         value={sortType}
         onChange={setSortType}
         optionList={sortOptionList}
       />
+      {/* 감정 filter select */}
+      <ControlMenu
+        value={emotionFilter}
+        onChange={setEmotionFilter}
+        optionList={emotionFilterList}
+      />
+
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <div key={it.id}>
+          {it.content}
+          {it.emotion}
+        </div>
       ))}
     </div>
   );
