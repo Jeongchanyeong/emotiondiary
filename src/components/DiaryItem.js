d033f03036de58501d27a6a4860e1ae50562e062
Jeongchanyeong <135792468020@naver.com>
2024-01-06 02:26:24 +0900

    Feat/ DiaryItem form 완성, navigate 연결

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,7 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
+
 const DiaryItem = ({ id, emotion, content, date }) => {
+  const strDate = new Date(parseInt(date)).toLocaleString();
+
+  const navigate = useNavigate();
+
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
   return (
     <div className="DiaryItem">
       <div
+        onClick={goDetail}
         className={[
           "emotion_ing_wrapper",
           `emotion_ing_wrapper_${emotion}`,
@@ -9,8 +23,13 @@ const DiaryItem = ({ id, emotion, content, date }) => {
       >
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
       </div>
-      <div></div>
-      <div></div>
+      <div className="info_wrapper" onClick={goDetail}>
+        <div className="diary_date">{strDate}</div>
+        <div className="diary_content_preview">{content.slice(0, 25)}</div>
+      </div>
+      <div className="btn_wrapper">
+        <MyButton onClick={goEdit} text={"수정하기"} />
+      </div>
     </div>
   );
 };
